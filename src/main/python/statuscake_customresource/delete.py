430e6b15ac65e95bd2cb3af99925a3585f854607
Sebastian Spoerer <sebastian.spoerer@immobilienscout24.de>
2016-02-10 13:29:29 +0100

    more debug

--- src/main/python/statuscake_customresource/delete.py
+++ src/main/python/statuscake_customresource/delete.py
@@ -10,10 +10,12 @@ def delete_status_cake(event):
     r = requests.delete('https://www.statuscake.com/API/Tests/Details', data=parameters, headers=headers)
     response = r.text
 
-    output = {'StackId': event['StackId'],
-              'RequestId': event['RequestId'],
-              'LogicalResourceId': event['LogicalResourceId'],
-              'PhysicalResourceId': event['PhysicalResourceId']}
+    output = {
+        'StackId': event['StackId'],
+        'RequestId': event['RequestId'],
+        'LogicalResourceId': event['LogicalResourceId'],
+        'PhysicalResourceId': event['PhysicalResourceId']
+    }
     output['Status'] = 'SUCCESS'
 
     requests.put(event['ResponseURL'], data=json.dumps(output))
