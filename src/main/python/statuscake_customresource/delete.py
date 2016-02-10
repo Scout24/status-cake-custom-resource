c50bd05c5e7fd831cce9eafad6af9eaabca819a2
Sebastian Spoerer <sebastian.spoerer@immobilienscout24.de>
2016-02-10 12:57:11 +0100

    typo

--- src/main/python/statuscake_customresource/delete.py
+++ src/main/python/statuscake_customresource/delete.py
@@ -6,7 +6,7 @@ def delete_status_cake(event):
     properties = event['ResourceProperties']
     test_id = 12345
     parameters = {'TestID': test_id}
-    headers = {'API': properties['apiKey'], 'Username': properties['userName']}
+    headers = {'API': properties['ApiKey'], 'Username': properties['UserName']}
     r = requests.delete('https://www.statuscake.com/API/Tests/Details', data=parameters, headers=headers)
     response = r.text
 
